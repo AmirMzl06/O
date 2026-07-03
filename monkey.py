3684783f62846904d72c4bfc12eabe660bb866e9
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-07-04 00:41:29 +0330

    Update monkey.py

--- monkey.py
+++ monkey.py
@@ -1,3 +1,7 @@
-from scipy.io import loadmat
-mat = loadmat("monkey_dataset/Jango_20150730_001.mat")
-print(mat.keys())
+import h5py
+
+f = h5py.File("monkey_dataset/Jango_20150730_001.mat", "r")
+
+print("Top-level keys:")
+for k in f.keys():
+    print(k)
