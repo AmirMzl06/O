dfa9236fba277f52dfefe4f3ef9648a4df6079b6
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-07-04 00:42:18 +0330

    Update monkey.py

--- monkey.py
+++ monkey.py
@@ -2,6 +2,7 @@ import h5py
 
 f = h5py.File("monkey_dataset/Jango_20150730_001.mat", "r")
 
-print("Top-level keys:")
-for k in f.keys():
-    print(k)
+xds = f["xds"]
+
+print(type(xds))
+print(xds.keys())
