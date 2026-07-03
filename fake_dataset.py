84d34632bac4035a40eb451a643a23d3008d5c17
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-07-03 19:52:13 +0330

    Update fake_dataset.py

--- fake_dataset.py
+++ fake_dataset.py
@@ -203,6 +203,15 @@ def compute_attribution(
         "jfconvabsinv": jfconvabsinv,
     }
 
+def setup_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 def save_ground_truth_plot(
     ground_truth,
     clean_result,
