a46591a6c938d799cef5c6f68c532d377eccc83d
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-07-04 00:46:12 +0330

    Update monkey.py

--- monkey.py
+++ monkey.py
@@ -4,5 +4,16 @@ f = h5py.File("monkey_dataset/Jango_20150730_001.mat", "r")
 
 xds = f["xds"]
 
-print(type(xds))
-print(xds.keys())
+for key in [
+    "spike_counts",
+    "EMG",
+    "force",
+    "curs_p",
+    "trial_target_dir",
+    "unit_names",
+]:
+    obj = xds[key]
+    print(f"\n{key}")
+    print(type(obj))
+    print(obj.shape)
+    print(obj.dtype)
