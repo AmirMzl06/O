426b62199182bc7a621bb79b1dfa5154fd24d25d
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-07-11 14:47:21 +0330

    Update interpretability.py

--- interpretability.py
+++ interpretability.py
@@ -18,8 +18,12 @@ from scipy.ndimage import gaussian_filter1d
 DATASET_DIR = "monkey_dataset"
 
 sessions = [
+    Path(DATASET_DIR) / "Jango_20150730_001.mat",
     Path(DATASET_DIR) / "Jango_20150731_001.mat",
+    Path(DATASET_DIR) / "Jango_20150801_001.mat",
+    Path(DATASET_DIR) / "Jango_20150805_001.mat",
     Path(DATASET_DIR) / "Jango_20150806_001.mat",
+    Path(DATASET_DIR) / "Jango_20150807_001.mat",
 ]
 
 RESULT_DIR   = "results_monkey_recon"
